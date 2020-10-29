22ca1353d19aaf54440b95db9e64daf80b9d6b1f
leonlimsc <leon.li@mattel.com>
2020-10-29 13:15:43 +0800

    more code in dev

--- main.cpp
+++ main.cpp
@@ -11,5 +11,9 @@ void something_added(void) {
 void add_in_dev_branch(void) {
     std::cout<<"this is added in develop branch\n";
 }
+
+void more_code_in_dev(void) {
+    std::cout<<"good in dev\n";
+}
     
     
